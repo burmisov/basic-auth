b88ef96e4f3fe7c95eaa71680f97958ccd3551f7
tetarenko <eugene.tetarenko@gmail.com>
2016-07-25 21:48:53 +0300

    настройки по-умолчанию и передача параметров при создании сессии

--- src/lib/cookiesSession.js
+++ src/lib/cookiesSession.js
@@ -1,9 +1,13 @@
 import session from 'express-session';
 
-export default function () {
-  return [session({
+export default function (options) {
+  const defaultOptions = {
     secret: 'some secret',
     resave: true,
     saveUninitialized: true,
+  };
+  return [session({
+    ...defaultOptions,
+    ...options,
   })];
 }
